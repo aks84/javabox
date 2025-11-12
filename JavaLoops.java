86427d2300a939a4d29b124fc00aef7fa7f862e7
aks84 <amulyaa_24@yahoo.in>
2025-11-12 16:30:35 +0530

    fixed for loop

--- JavaLoops.java
+++ JavaLoops.java
@@ -6,7 +6,7 @@ public class JavaLoops{
 		String[] months = {"January", "February", "March", "April", "May", "June", "Jyly", "August", "September", "October", "Novermber", "December"};
 		System.out.println("\tfor loop");
 
-		for(int i=0; i <= months.length; i++){
+		for(int i=0; i < months.length; i++){
 			System.out.println(months[i]);
 		}
 
